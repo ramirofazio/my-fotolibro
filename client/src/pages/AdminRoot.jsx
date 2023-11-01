213492ae59154c6c40ed8f14a869989023649404
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-11-01 13:07:46 -0300

    a

--- client/src/pages/AdminRoot.jsx
+++ client/src/pages/AdminRoot.jsx
@@ -3,7 +3,7 @@ import {AdminNav} from "../components/AdminNav.jsx"
 
 export function AdminRoot() {
   return (
-    <div className="bg-main flex flex-col justify-between min-h-screen">
+    <div className="bg-base flex flex-col justify-between min-h-screen">
     <AdminNav/>
     <Outlet/>
     </div>
