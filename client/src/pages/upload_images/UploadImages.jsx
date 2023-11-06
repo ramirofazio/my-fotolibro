3bd9007a21952ebefd9624a3900057b782ff8a93
jhoniernem <jhonny30ca@gmail.com>
2023-11-06 18:54:02 -0500

    Feat add images option

--- client/src/pages/upload_images/UploadImages.jsx
+++ client/src/pages/upload_images/UploadImages.jsx
@@ -17,7 +17,8 @@ export function UploadImages() {
           ...cur,
           {
             name: file.name,
-            image: typeof reader.result === 'string' ? reader.result : '',
+            index: i,
+            url: typeof reader.result === 'string' ? reader.result : '',
             file: file,
           },
         ]);
@@ -26,7 +27,7 @@ export function UploadImages() {
     }
   }
   return (
-    <div className="">
+    <div className="p-6">
       <div className="text-white border-dashed border relative overflow-hidden flex flex-col justify-center items-center  p-4">
         <p className="text-lg font-medium mb-2">Arrastra aquí tus imagenes</p>
         <label
@@ -44,54 +45,41 @@ export function UploadImages() {
           onChange={handleImages}
         />
       </div>
-      <button onClick={() => null} className="">
-        Test
-      </button>
-      <div className="p-4">
+      <div className="mt-4 grid grid-cols-3 gap-4">
         {images.map((image, i) => (
           <div key={i} className="relative w-fit">
             <img
-              src={image.image}
+              src={image.url}
               alt="a image"
               key={i}
               className="w-[500px] aspect-square"
             />
-            <button className="absolute top-2 right-2 text-lg">x</button>
+            <button className="absolute top-2 right-2 w-7 h-7 hover:text-red-600 rounded-full hover:bg-gray-400/40" title="Eliminar">
+              <XIcon />
+            </button>
           </div>
         ))}
       </div>
     </div>
   );
 }
-/**
-  const imageOnChange = async (e) => {
 
-  const { target } = e;
-
-  const file = target.files[0];
-
-  const reader = new FileReader();
-  reader.onload = () => {
-    setNewImg({
-      name: file.name,
-      image: typeof reader.result === "string" ? reader.result : "",
-      file: file,
-    });
-    setIsUserChanged(true);
-  };
-  reader.readAsDataURL(file);
-};
-
-function handleUpload () {
-  if (newImg) {
-    const formdata = new FormData();
-    formdata.append("file", newImg.image);
-    formdata.append("userId", userId);
-
-    const res = await axios.post(`http://localhost:3000/cloudinary/updateAvatar`, formdata, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
-    newAvatar = res.data;
-  }
+function XIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M18 6L6 18M6 6l12 12"
+      />
+    </svg>
+  );
 }
- */
