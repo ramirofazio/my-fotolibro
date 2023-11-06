19fd48a912d3bde53f216e71ccea1e94c8c2f8f5
jhoniernem <jhonny30ca@gmail.com>
2023-11-06 12:23:14 -0500

    feat upload images to cloudinary ...

--- client/src/pages/upload_images/UploadImages.jsx
+++ client/src/pages/upload_images/UploadImages.jsx
@@ -1,21 +1,104 @@
+import { useState } from 'react';
+
 export function UploadImages() {
+  const [images, setImages] = useState([]);
+
+  function removeImage(index) {
+    const Images = images.filter((_img, i) => i !== index);
+    setImages(Images);
+  }
+
+  function handleImages({ target }) {
+    const files = target.files;
+    if (!files) return;
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      if (!file) continue;
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImages((cur) => [
+          ...cur,
+          {
+            name: file.name,
+            image: typeof reader.result === 'string' ? reader.result : '',
+            file: file,
+          },
+        ]);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
   return (
     <div className="">
       <div className="text-white border-dashed border relative overflow-hidden flex flex-col justify-center items-center  p-4">
-        <p className="text-lg font-medium">Arrastra aquí tus imagenes</p>
-        <label htmlFor="upload-images" className="w-fit">
-          <button className="cursor-pointer  bg-blue-700 px-5 py-3 rounded">
-            Seleccionar Imagenes
-          </button>
+        <p className="text-lg font-medium mb-2">Arrastra aquí tus imagenes</p>
+        <label
+          htmlFor="upload-images"
+          className="w-fit cursor-pointer bg-blue-700 px-5 py-3 rounded hover:font-medium"
+        >
+          Seleccionar Imagenes
         </label>
         <input
           className="invisible w-full h-full absolute top-0 right-0"
           id="upload-images"
           type="file"
           accept="image/*"
-          multiple=""
+          multiple
+          onChange={handleImages}
         />
+      </div>
+      <div className="p-4">
+        {images.map((image, i) => (
+          <div key={i} className="relative w-fit">
+            <img
+              src={image.image}
+              alt="a image"
+              key={i}
+              className="w-[500px] aspect-square"
+            />
+            <button
+              onClick={removeImage}
+              className="absolute top-2 right-2 text-lg"
+            >
+              x
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
+/**
+  const imageOnChange = async (e) => {
+
+  const { target } = e;
+
+  const file = target.files[0];
+
+  const reader = new FileReader();
+  reader.onload = () => {
+    setNewImg({
+      name: file.name,
+      image: typeof reader.result === "string" ? reader.result : "",
+      file: file,
+    });
+    setIsUserChanged(true);
+  };
+  reader.readAsDataURL(file);
+};
+
+function handleUpload () {
+  if (newImg) {
+    const formdata = new FormData();
+    formdata.append("file", newImg.image);
+    formdata.append("userId", userId);
+
+    const res = await axios.post(`http://localhost:3000/cloudinary/updateAvatar`, formdata, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    newAvatar = res.data;
+  }
+}
+ */
---
+++ client/src/utils/index.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export async function uploadImagesCloudinary(files = []) {
+  const cloud_name = import.meta.env.CLOUD_NAME || '';
+  const preset_key = import.meta.env.PRESET_KEY || '';
+
+  const URL = 'https://api.cloudinary.com/v1_1/' + cloud_name + '/image/upload';
+
+  const images_urls = [];
+  const promises = [];
+
+  files.forEach((file) => {
+    const formdata = new FormData();
+    formdata.append('file', file);
+    formdata.append('upload_preset', preset_key);
+    promises.push(axios.post(URL, formdata));
+  });
+
+  try {
+    const responses = await Promise.all(promises);
+    console.log(promises[0]);
+    responses.forEach((res) => {
+      const url = res?.data?.secure_url;
+      if (url) {
+        images_urls.push(url);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  return images_urls;
+}
