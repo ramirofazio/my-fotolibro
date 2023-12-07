825d5f66af93475f938c8258053f67f787400acd
Your Name <ramifazio@gmail.com>
2023-12-07 15:49:55 -0300

    fix

--- client/src/api_instance/base_api.js
+++ client/src/api_instance/base_api.js
@@ -3,12 +3,9 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-  maxBodyLength: 750000000, // hacerlo en post con el header form-data
+  maxBodyLength: 750000000,
   baseURL: API_URL,
   headers: {
-    // ? Configuracion las cabeceras CORS
-    "Access-Control-Allow-Origin": import.meta.env.VITE_ACCESS_URL,
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, DELETE",
-    //"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
   },
 });
