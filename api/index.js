f0cba8913deaeba07ceb4227b87b1f42a2641e4e
SourerDev <jhonny30ca@gmail.com>
2024-03-06 17:02:15 -0500

    updated file

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
-const { check_env, env } = require('./src/utils')
-const server = require('./src/app.js')
-const { conn } = require('./src/db.js')
+const { check_env, env } = require("./src/utils");
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
 
 function main(force) {
-  const PORT = env.PORT || 3001
+  const PORT = env.PORT || 3001;
 
-  check_env()
+  check_env();
 
   conn.sync({ force, alter: !force }).then(() => {
-    server.listen(PORT, () => console.log('%s listening at ' + PORT))
-  })
+    server.listen(PORT, () => console.log("%s listening at " + PORT));
+  });
 }
 
-main(true)
+main(true);
