93b19ce44668188b6f5638cd71a217656b24770f
SourerDev <jhonny30ca@gmail.com>
2024-03-06 16:00:14 -0500

    feat: env.js file -check variables

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
+const { check_env } = require('./src/utils')
+
+/*
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ force: false, alter: true }).then(() => {
+ conn.sync({ force: false, alter: true }).then(() => {
   server.listen(3001, () => {
     console.log("%s listening at 3001");
   });
-});
+}); */
+
+function main() {
+  check_env()
+}
+
+main()
+
+console.log('good')
---
+++ api/src/utils/env.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const {
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME,
+  DATABASE_URL,
+  CLIENT_URL,
+} = process.env
+
+module.exports = {
+  env: {
+    cloudinary: {
+      API_KEY: CLOUDINARY_API_KEY,
+      API_SECRET: CLOUDINARY_API_SECRET,
+      CLOUD_NAME: CLOUDINARY_CLOUD_NAME,
+    },
+    DATABASE_URL,
+    CLIENT_URL,
+  },
+  check_env: function () {
+    const errors = []
+
+    if (!DATABASE_URL) errors.push('DATABASE_URL: [REQUIRED]')
+    if (!CLOUDINARY_API_KEY) errors.push('CLOUDINARY_API_KEY: [REQUIRED]')
+    if (!CLOUDINARY_API_SECRET) errors.push('CLOUDINARY_API_SECRET: [REQUIRED]')
+    if (!CLOUDINARY_CLOUD_NAME) errors.push('CLOUDINARY_CLOUD_NAME: [REQUIRED]')
+    if (!CLIENT_URL) errors.push('CLIENT_URL: [REQUIRED]')
+
+    if (errors.length) {
+      console.log('\nvariables are missing in the .env file.')
+      errors.forEach((err) => console.log(err))
+      console.log('')
+      throw 'Something went wrong!'
+    }
+  },
+}
